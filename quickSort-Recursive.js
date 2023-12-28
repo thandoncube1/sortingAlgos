6f3dee9e898f7c97b21ad7df4c77748b4d3c47c4
thandon263 <ncubethando20@gmail.com>
2023-12-27 23:15:50 -0500

    Implementing the quickSort the simplest possible solution.

---
+++ quickSort-Recursive.js
@@ -0,0 +1,12 @@
+let array = [12, 43, 64, 2, 5, 8, 16, 128, 19, 10, 3, 1, 25];
+
+function quickSort(arr) {
+    if (!arr || arr.length <= 1) return arr; // If the array is empty, return an empty array
+    let pivot = arr[0]; // Set the pivot point of the array
+    let left = arr.filter(x => x < pivot); // Filter values that are less than the pivot point
+    let right = arr.filter(x => x > pivot); // Filter values that are greater than the pivot point
+    return [...quickSort(left), pivot, ...quickSort(right)]; // Return the array, spread the quickSort values that are less than the pivot point and values that are greater than the pivot point.
+}
+
+let result = quickSort(array);
+console.log(result);
